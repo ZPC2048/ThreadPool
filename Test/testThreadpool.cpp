bf46009ab342e3536521b856df979ba9d125926d
ZPC2048 <zpc2048@qq.com>
2023-08-02 00:47:19 +0800

    init

--- Test/testThreadpool.cpp
+++ Test/testThreadpool.cpp
@@ -1,11 +1,28 @@
 #include "threadpool.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <string>
 
-int f(int x, int y) {
-  return x + y;
+void f(int sleepTime, int iterateCount) {
+  sleep(sleepTime);
+  int tid = gettid();
+  for (int i = 0; i < iterateCount; ++i) {
+    std::string s = "[thread id " + std::to_string(tid) + "] " + "output " + std::to_string(i) + "\n";
+    std::cout << s;
+  }
 }
 
 int main() {
-  ThreadPool threadPool;
-  threadPool.submitTask(f, 1, 2);
-  
+  std::cout << "thread pool created" << std::endl;
+  {
+    ThreadPool threadPool(5);
+    threadPool.submitTask(f, 0, 3);
+    threadPool.submitTask(f, 0, 3);
+    threadPool.submitTask(f, 0, 3);
+    threadPool.submitTask(f, 0, 3);
+    threadPool.submitTask(f, 1, 3);
+  }
+  std::cout << "thread pool destroyed" << std::endl;
+  sleep(3);
+  return 0;
 }
--- threadpool.hpp
+++ threadpool.hpp
@@ -8,54 +8,74 @@
 class ThreadPool {
 public:
 
-  explicit ThreadPool(const int threadPoolSie = 10) {
-    threads.resize(threadPoolSie);
-  }
-
-  ~ThreadPool() {
-    closed = true;
-    conditionVariable.notify_all();
-    for (auto& i : threads) {
-      if (i.joinable()) {
-        i.join();
-      }
+  explicit ThreadPool(const int threadPoolSie = 10) :
+    threadPoolPtr(std::make_shared<ThreadPoolPtr>()) {
+    threadPoolPtr->threads.resize(threadPoolSie);
+    threadPoolPtr->closed = false;
+    for (int i = 0; i < threadPoolSie; ++i) {
+      threadPoolPtr->threads[i] = std::move(std::thread(ThreadWorker(i, threadPoolPtr)));
+      threadPoolPtr->threads[i].detach();
     }
   }
 
+  ~ThreadPool() {
+    {
+      std::unique_lock<std::mutex> poolMutex(threadPoolPtr->poolMutex);
+      threadPoolPtr->closed = true;
+    }
+    threadPoolPtr->conditionVariable.notify_all();
+  }
+
   template<typename F, typename... Args>
-  auto submitTask(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
-    std::function<decltype(f(args...))> func = std::bind(std::forward(f), std::forward(args), std::forward(...));
-    auto funcPtr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+  auto submitTask(F&& f, Args&&... args) -> std::future<decltype(std::forward<F>(f)(std::forward<Args>(args)...))> {
+    using returnType = decltype(std::forward<F>(f)(std::forward<Args>(args)...));
+    std::function<returnType()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+    auto funcPtr = std::make_shared<std::packaged_task<returnType()>>(func);
     auto wrappedFunction = [funcPtr]() {
       (*funcPtr)();
     };
-    taskQueue.emplace(wrappedFunction);
-    conditionVariable.notify_one();
+    {
+      std::unique_lock<std::mutex> poolMutex(threadPoolPtr->poolMutex);
+      threadPoolPtr->taskQueue.emplace(wrappedFunction);
+    }
+    threadPoolPtr->conditionVariable.notify_one();
     return funcPtr->get_future();
   }
 
 protected:
+  struct ThreadPoolPtr {
+    bool closed;
+    std::mutex poolMutex;
+    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> taskQueue;
+    std::condition_variable conditionVariable;
+  };
+
   class ThreadWorker {
   public:
-    ThreadWorker(int threadId, ThreadPool* pool) :
+    ThreadWorker(int threadId, std::shared_ptr<ThreadPoolPtr> pool) :
       threadId(threadId), pool(pool) {}
     
     void operator() () {
-      while (!pool->closed) {
-        std::unique_lock<std::mutex> threadMutex(pool->threadMutex);
-        while (pool->taskQueue.empty()) {
-          pool->conditionVariable.wait(threadMutex);
+      std::function<void()> f;
+      std::unique_lock<std::mutex> poolMutex(pool->poolMutex);
+      while (true) {
+        if (!pool->taskQueue.empty()) {
+          f = std::move(pool->taskQueue.front());
+          pool->taskQueue.pop();
+          poolMutex.unlock();
+          f();
+          poolMutex.lock();
+        } else if (pool->closed) {
+          break;
+        } else {
+          pool->conditionVariable.wait(poolMutex);
         }
-
       }
     }
     int threadId;
-    ThreadPool* pool;
+    std::shared_ptr<ThreadPoolPtr> pool;
   };
 
-  bool closed;
-  std::mutex threadMutex;
-  std::vector<std::thread> threads;
-  std::queue<std::function<void()>> taskQueue;
-  std::condition_variable conditionVariable;
+  std::shared_ptr<ThreadPoolPtr> threadPoolPtr;
 };
